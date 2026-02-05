73d382b894495d6650a23c6c07add7973f5dfbef
Edwin <edwin313560@gmail.com>
2026-02-04 21:38:33 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -24,8 +24,25 @@ global.sticker = {
   author: 'By Edwin'
 }
 
+// --- LINKS GLOBALES (para links invisibles, previews, etc) ---
+global.links = {
+  socials: [
+    'https://instagram.com/tu_ig',
+    'https://tiktok.com/@tu_tiktok'
+  ],
+  group: [
+    'https://chat.whatsapp.com/TU_GRUPO_PRINCIPAL'
+  ],
+  all: [
+    'https://instagram.com/tu_ig',
+    'https://tiktok.com/@tu_tiktok',
+    'https://chat.whatsapp.com/TU_GRUPO_PRINCIPAL'
+  ]
+}
+
 module.exports = {
   config: global.config,
   messages: global.messages,
   sticker: global.sticker,
+  links: global.links
 }
